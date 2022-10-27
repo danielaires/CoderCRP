5198d388a589be66679ad63dddfffa1b18f87d3b
danielaires <danielaires95@gmail.com>
2022-10-27 16:26:05 -0300

    Crud de artista

--- src/crp/registroartista/controller/ArtistaController.java
+++ src/crp/registroartista/controller/ArtistaController.java
@@ -1,5 +1,49 @@
 package crp.registroartista.controller;
 
-public class ArtistaController {
+import java.util.ArrayList;
+import java.util.List;
 
+import crp.registroartista.model.Artista;
+
+public class ArtistaController {
+	
+	
+	List<Artista> lista = new ArrayList();
+	
+	
+	public void buscarPorId(Integer id) {
+		
+		System.out.println(lista.get(id).toString());
+		
+	}
+	
+	public void adicionarArtista(Artista artista) {
+		
+		System.out.println(lista.add(artista));
+	}
+	
+	
+	public void removerArtista(Artista artista) {
+		
+		if(artista.getId()!= null && artista.getId()>0) {
+			
+			lista.remove(artista);
+			System.out.println("Artista excluindo com sucesso!!");
+		}
+		
+	}
+	
+	public void alterarArtista(Artista artista) {
+		
+		if(artista.getId() != null && artista.getId()>0) {
+			
+			int index = lista.indexOf(artista);
+			lista.add(index, artista);
+			
+			return;
+			
+		}
+	
+	}
+	
 }
